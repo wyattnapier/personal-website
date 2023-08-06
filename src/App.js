6cf756e4fbfc6a2a1bc3b2737c263d41f63dd59b
Wyatt <wnapier@bu.edu>
2023-08-05 20:07:41 -0400

    tidying code

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import Extracurr from './Extracurr';
 
 
 const Button = ({action, text}) => {
-  console.log('derp');
-  //need display function to show feedback values
   return (
     <button onClick={action}>
       <p>{text}</p>
@@ -21,24 +19,12 @@ const Button = ({action, text}) => {
   )
 }
 
-
-const DisplayVal = ({val}) => {
-  console.log(val)
-  return (
-    <h3>{val}</h3>
-  )
-}
-
 const App = () => {
-  const [feedback, setFeedback] = useState(0);
-  const incrementFeedback = () => { setFeedback(feedback + 1) }
-
+  
   const [reveal, setReveal] = useState(false);
   function handleReveal () {
     setReveal(!reveal);
   }
-  // setReveal = useState(true);
-  console.log("OG reveal: ", reveal);
   
   return (
     <div className="App">
@@ -58,42 +44,3 @@ const App = () => {
 }
 
 export default App;
-
-/*
-trash HTML heap
-
-<div className="HeaderRow">
-        <span>  
-          <h2>This is me!</h2> 
-          <p>some more sample text to double check, but let's say it was really long</p>  
-        </span>
-        <p>I love playing around</p>
-        <img src={WyattTree} className="App-WyattTree" alt="Wyatt Napier" />
-      </div>
-
-
-
-      <p>the boolean value is: {reveal}</p>
-      <DisplayVal val={reveal} />
-
-      <Button action={incrementFeedback} text={"like"} />
-      <DisplayVal val={feedback}/>
-      {reveal && <p>I miss my friends</p>}
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-
-*/
--- src/Extracurr.js
+++ src/Extracurr.js
@@ -17,7 +17,6 @@ export default function Extracurr () {
                     </li>
                 </ul>
             ))}
-            rahhhhh so extra
         </div>
     )
 }
